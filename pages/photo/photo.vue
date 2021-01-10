8896f369b3e5978643f6c6f430b56ac145c1e705
zjkmanng <804718924@qq.com>
2021-01-10 14:33:29 +0800

    photo

---
+++ pages/photo/photo.vue
@@ -0,0 +1,50 @@
+<template>
+	<view class="content">
+		<view class="name">
+			火场照相提示
+		</view>
+		
+		<view class="home" @click="home">
+			首页
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		methods: {
+			home() {
+				uni.switchTab({
+					url: '../../index/index'
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content{
+		padding: 20rpx 20rpx 150rpx;
+		position: relative;
+		.name{
+			font-size: 38rpx;
+			font-weight: bold;
+			line-height: 100rpx;
+		}
+		
+		.home{
+			position: fixed;
+			left: 0;
+			bottom: 0;
+			width: 100%;
+			height: 100rpx;
+			border-top: 1px solid #ccc;
+			background-color: #fff;
+			display: flex;
+			align-items: center;
+			text-align: center;
+			justify-content: center;
+			z-index: 999;
+		}
+	}
+</style>
